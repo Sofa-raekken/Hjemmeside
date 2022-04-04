13afc69d9c42b5a58d446ad0a5e9d1e61c384fcf
NilasThoegersen <nilas96@gmail.com>
2022-04-04 15:11:58 +0200

    Create files for login etc.

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2 class="text-4xl center">Login</h2>
+    <label for="LoginUsername" class="text-xl">Username</label>
+    <input type="text" v-model="username" name="Username" id="Username"/>
+    <label for="LoginPassword" class="text-xl">Password</label>
+    <input type="password" v-model="password" name="LoginPassword" id="LoginPassword"/>
+    <button @click="submit">Log ind</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data () {
+    return {
+      password:"",
+      username:""
+    }
+  },
+  methods: {
+    submit () {
+      this.$http.post("https://login.microsoftonline.com/1f1d0810-cd59-4055-868b-fd8c0739f409/oauth2/v2.0/authorize", )
+    }
+  }
+};
+</script>
+
+<style></style>
