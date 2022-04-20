8dd911104c6a6f8ed559db1a2588605d80c3a1ed
NilasThoegersen <nilas96@gmail.com>
2022-04-20 14:53:58 +0200

    Animal Logic
    All animal logic done. Modal needs styling

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import Emitter from 'tiny-emitter'
 import store from './store'
 import './index.css'
 import axios from 'axios'
+import 'vue-universal-modal/dist/index.css'
+import VueUniversalModal from 'vue-universal-modal'
 
 const app = createApp({
   render: () => h(App)
@@ -13,4 +15,7 @@ const app = createApp({
 app.config.globalProperties.$msalInstance = {}
 app.config.globalProperties.$emitter = new Emitter()
 app.config.globalProperties.$axios = axios
-app.use(store).use(router).mount('#app')
+
+app.use(store).use(VueUniversalModal, {
+  teleportTarget: '#modals'
+}).use(router).mount('#app')
--- src/views/AnimalView.vue
+++ src/views/AnimalView.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
   <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-    <button class="float-right rounded bg-indigo-500 p-1 mb-3 mr-4 text-white font-semibold">Opret nyt dyr</button>
+    <button class="float-right rounded bg-indigo-500 p-1 mb-3 mr-4 text-white font-semibold" @click="showModal">Opret nyt dyr</button>
 
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
       <thead
@@ -38,6 +38,43 @@
       </tbody>
     </table>
   </div>
+  <Modal
+    v-model="isShow"
+    :close="closeModal"
+    class=""
+    >
+    <div class="bg-white w-3/4 h-3/4">
+      <h2 class="center text-4xl">
+        {{modalType}}
+      </h2>
+      <div>
+        <label for="name" class="">Navn</label>
+        <input type="text" class="" id="nameInput" v-model="animal.name">
+        <label for="latinName" class="">Latinsk navn</label>
+        <input type="text" class="" id="latinNameInput" v-model="animal.latinName">
+        <label for="animalBirthweight">Fødselsvægt</label>
+        <input id="animalBirthweight" type="text" class=""/>
+        <label for="description" class="">Beskrivelse</label>
+        <input type="text" class="" id="descriptionInput" v-model="animal.description">
+        <label for="height" class="">Højde</label>
+        <input type="text" class="" id="heightInput" v-model="animal.height">
+        <label for="latinName" class="">Latinsk navn</label>
+        <input type="text" class="" id="latinNameInput" v-model="animal.latinName">
+        <label for="lifeExpectancy" class="">Levetid</label>
+        <input type="text" class="" id="lifeExpectancyInput" v-model="animal.lifeExpectancy">
+        <label for="pregnancy" class="">Graviditetslængde</label>
+        <input type="text" class="" id="pregnancyInput" v-model="animal.pregnancy">
+        <label for="weight" class="">Vægt</label>
+        <input type="text" class="" id="weightInput" v-model="animal.weight">
+      </div>
+      <button @click="SendRequest()" class="">
+        {{modalType}}
+      </button>
+      <button @click="closeModal" class="">
+        close
+      </button>
+    </div>
+  </Modal>
 </div>
 </template>
 
@@ -56,8 +93,20 @@ export default {
   },
   data () {
     return {
+      isShow: false,
       animals: [],
-      animal: {}
+      animal: {
+        birthWeight: '',
+        description: '',
+        height: '',
+        latinName: '',
+        lifeExpectancy: '',
+        name: '',
+        pregnancy: '',
+        qr: '',
+        weight: ''
+      },
+      modalType: ''
     }
   },
   methods: {
@@ -67,12 +116,43 @@ export default {
       }
     },
     UpdateAnimal (objAnimal) {
-      axios.put('https://sofaapi.azurewebsites.net/animals/' + objAnimal.idAnimal, this.animal).then((response) => {
-        console.log(response.data)
-      })
+      this.animal = objAnimal
+      this.modalType = 'Opdater'
+      this.showModal()
+    },
+    CreateAnimal () {
+      this.animal = {
+        birthWeight: '',
+        description: '',
+        height: '',
+        latinName: '',
+        lifeExpectancy: '',
+        name: '',
+        pregnancy: '',
+        qr: '',
+        weight: ''
+      }
+      this.modalType = 'Opret'
+      this.showModal()
     },
     CreateQR (id) {
       axios.get('https://sofaapi.azurewebsites.net/animals/' + id).then(response => { console.log(response.data) }).catch(error => { console.log(error) })
+    },
+    SendRequest () {
+      if (this.animal.species === null) {
+        this.animal.species = 1
+      }
+      if (this.modalType === 'Opdater') {
+        axios.put('https://sofaapi.azurewebsites.net/animals/' + this.animal.idAnimal, this.animal).then((response) => { console.log(response.data) })
+      } else {
+        axios.post('https://sofaapi.azurewebsites.net/animals/' + this.animal.idAnimal, this.animal).then((response) => { console.log(response.data) })
+      }
+    },
+    showModal () {
+      this.isShow = true
+    },
+    closeModal () {
+      this.isShow = false
     }
   }
 }
