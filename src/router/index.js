9656eafc9ccf458630191c1c7a3fa728402a6ed4
NilasThoegersen <nilas96@gmail.com>
2022-04-20 15:31:04 +0200

    Save before working at home

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/EventView.vue')
+  },
+  {
+    path: '/feedback',
+    name: 'feedback',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/FeedbackView.vue')
   }
 ]
 
--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,48 +1,98 @@
 <template>
-  <div>
+<div>
+  <div class="grid grid-cols-4 gap 3">
     <div>
-      <button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
-        Toggle modal
-      </button>
-
-      <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-        <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-          <!-- Modal content -->
-          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex justify-between items-start p-5 rounded-t border-b dark:border-gray-600">
-              <h3 class="text-xl font-semibold text-gray-900 lg:text-2xl dark:text-white">
-                Terms of Service
-              </h3>
-              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
-                <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"/>
-                </svg>
-              </button>
-            </div>
-            <!-- Modal body -->
-            <div class="p-6 space-y-6">
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                  With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-              </p>
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                  The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-              </p>
-            </div>
-            <!-- Modal footer -->
-            <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
-              <button data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-              <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    </div>
+    <div>
+    </div>
+    <div>
+    </div>
+    <div>
+      <select>
+        <option v-for="animal in animals" :key="animal.idAnimal">
+          {{animal.name}}
+        </option>
+      </select>
     </div>
   </div>
+  <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+    <thead
+      class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"      >
+      <tr>
+        <th scope="col" class="px-6 py-3">Navn</th>
+        <th scope="col" class="px-6 py-3">Tidspunkt</th>
+        <th scope="col" class="px-6 py-3 center" colspan="2">Actions</th>
+        <th scope="col" class="px-6 py-3">Dyr</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="border-b dark:bg-gray-800 dark:border-gray-700 odd:bg-white even:bg-gray-50 odd:dark:bg-gray-800 even:dark:bg-gray-700" v-for="objAnimal in animals" :key="objAnimal.idAnimal">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+          {{objAnimal.name}}
+        </th>
+        <td class="px-6 py-4">{{objAnimal.Species}}</td>
+        <td class="px-6 py-4">
+          <button v-on:click="UpdateAnimal(objAnimal)">
+            Edit
+          </button>
+        </td>
+        <td class="px-6 py-4">
+          <button v-on:click="DeleteAnimal(objAnimal.idAnimal, objAnimal.name)">
+            Delete
+          </button>
+        </td>
+        <td>
+          <button class="button">
+            Download
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      events: [],
+      animals: [],
+      event: {}
+    }
+  },
+  methods: {
+    init () {
+      this.$axios.get('https://sofaapi.azurewebsites.net/events').then(response => { this.events = response.data }).catch(err => { console.log(err) })
+      this.$axios.get('https://sofaapi.azurewebsites.net/animals').then(response => { this.animals = response.data }).catch(err => { console.log(err) })
+    },
+    UpdateAnimal (objAnimal) {
+      this.animal = objAnimal
+      this.modalType = 'Opdater'
+      this.showModal()
+    },
+    CreateAnimal () {
+      this.animal = {
+        birthWeight: '',
+        description: '',
+        height: '',
+        latinName: '',
+        lifeExpectancy: '',
+        name: '',
+        pregnancy: '',
+        qr: '',
+        weight: ''
+      }
+      this.modalType = 'Opret'
+      this.showModal()
+    },
+    SendRequest () {
+      if (this.modalType === 'Opdater') {
+        this.$axios.put('https://sofaapi.azurewebsites.net/events/' + this.event.idEvent, this.event).then((response) => { console.log(response.data) })
+      } else {
+        this.$axios.post('https://sofaapi.azurewebsites.net/animals/' + this.event.idEvent, this.event).then((response) => { console.log(response.data) })
+      }
+    }
+  }
 }
 </script>
